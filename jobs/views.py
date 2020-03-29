dcd6c17b0b2e6c746f9c91d25df0bfa4eb61f916
Mahin Malhotra <mahinmalhotra20@gmail.com>
2020-03-30 00:32:18 +0530

    Job Homepage made

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Jobs
 
 # Create your views here.
+
+def home(request):
+
+    jobs=Jobs.objects
+    return render(request,'jobs/home.html',{'jobs':jobs})
